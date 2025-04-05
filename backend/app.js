b327adad435ccdcf64bc32932144a6b6518b3f73
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2025-04-05 14:03:39 -0400

    full auth

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
-import { signup, login } from './db.js';
+import { signup, login, upload } from './db.js';
+import multer from "multer";
 import express from 'express';
 import http from 'http';
 import fs from 'fs';
@@ -8,6 +9,7 @@ import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const uploader = multer({ storage: multer.memoryStorage() }); // Use memory storage for multer
 
 app.use(cors({
     origin: ['http://localhost:8080', 'https://localhost:3000'],
@@ -20,20 +22,34 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/v1/signup', (req, res) => {
-    console.log(req.body);
     const { email, password, username } = req.body;
     signup(email, password, username);
     res.status(200).json({ message: 'User created successfully' });
 });
 
-app.get('/api/v1/login', (req, res) => {
-    console.log(req.body);
+app.get('/api/v1/login', async (req, res) => {
     const { email, password } = req.body;
-    const response = login(email, password);
-    if(response){
-        res.status(200).json({ message: 'Login Sucessful' });
+    const response = await login(email, password);
+    if(response.success == true) {
+        res.status(200).json({ message: 'Login Sucessful', username: response.username });
     }else{
         res.status(401).json({ message: 'Invalid credentials' });
+    }
+});
+
+app.post('/api/v1/uploadCard', uploader.single('image'), async (req, res) => {
+    const file = req.file;
+    const username = req.body.username; // Replace with actual user ID logic
+
+    if (!file) {
+        return res.status(400).json({ success: false, message: "No file uploaded" });
+    }
+
+    const result = await upload(file, userId);
+    if (result.success) {
+        res.status(200).json({ success: true, downloadURL: result.downloadURL });
+    } else {
+        res.status(500).json({ success: false, message: result.message });
     }
 });
 
